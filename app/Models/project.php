6d52b717f6957710c696eca34e662d86c3cf7662
Yesho Simon <sepkap123@gmail.com>
2023-01-01 22:33:45 +0300

    added project migration

--- app/Models/project.php
+++ app/Models/project.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class project extends Model
 {
     use HasFactory;
+    protected $table = 'projects';
+    public $primaryKey = 'id';
+    public $timestamps = true;
+    protected $fillable = ['title'];
 }
