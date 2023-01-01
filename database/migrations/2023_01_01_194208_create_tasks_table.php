8436c75f626631e80142fa20b7c48c7115a7031a
Yesho Simon <sepkap123@gmail.com>
2023-01-01 22:44:05 +0300

    tasks migration

--- database/migrations/2023_01_01_194208_create_tasks_table.php
+++ database/migrations/2023_01_01_194208_create_tasks_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('completed')->default(0);
             $table->timestamps();
         });
     }
